7e95a740d3e7c5557efb742bcd0973f05277c1c1
root <garamgudang1917@gmail.com>
2021-03-16 14:35:34 -0400

    melakukan perubahan

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+	{
+		'author':'Muh Ahfandi H',
+		'title': 'Blog Post',
+		'content':'Ini Adalah Post Pertama',
+		'tanggal':'15 February 2021'
+	},
+	{
+		'author':'Muh Ahfandi H',
+		'title': 'Blog content',
+		'content':'Ini Adalah Post Kedua',
+		'tanggal':'16 February 2021'
+	},
+	{
+		'author':'Muh Ahfandi H',
+		'title': 'Blog about',
+		'content':'Ini Adalah Post Ketiga',
+		'tanggal':'17 February 2021'
+	}
+]
+
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	konteks = {
+		'kirim':posts
+	}
+	return render(request,'blog/home.html',konteks)
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request,'blog/about.html',{'title':'About'})
