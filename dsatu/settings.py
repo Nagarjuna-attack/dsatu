f9124807b6c17db953fe86db6c2eb3063f4ec60a
root <garamgudang1917@gmail.com>
2021-03-28 00:27:39 +0800

    sdf

--- dsatu/settings.py
+++ dsatu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['amigodjango.herokuapp.com']
+#ALLOWED_HOSTS = ['amigodjango.herokuapp.com']
 
 
 # Application definition
